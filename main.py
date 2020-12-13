b209f975547b47046d02f3d3a36e6cca18ee0937
john <john@jpatmore.plus.com>
2020-12-13 21:07:46 +0000

    display logo

--- main.py
+++ main.py
@@ -8,7 +8,15 @@ import tkinter as tk
 
 window = tk.Tk()
 window.title("Password Manager")
+window.config(padx=50, pady=50)
 
+canvas = tk.Canvas(width=200, height=200)
+image = tk.PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=image)
+
+
+# Grid layout
+canvas.grid(row=0, column=1)
 
 # ---------------------
 
