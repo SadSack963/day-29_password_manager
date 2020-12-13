7eb1d171d61d8e62bf0ea9b3a7c663f8fe6dd116
john <john@jpatmore.plus.com>
2020-12-13 22:32:50 +0000

    layout done, alignment using sticky=tk.*

--- main.py
+++ main.py
@@ -14,13 +14,13 @@ canvas = tk.Canvas(width=200, height=200)
 image = tk.PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=image)
 
-label_website = tk.Label(text="Website:", width=17)
-label_email = tk.Label(text="Email / Username:", width=17)
-label_password = tk.Label(text="Password:", width=17)
+label_website = tk.Label(text="Website:")
+label_email = tk.Label(text="Email / Username:")
+label_password = tk.Label(text="Password:")
 
-text_website = tk.Text(width=35, height=1)
-text_email = tk.Text(width=35, height=1)
-text_password = tk.Text(width=21, height=1)
+entry_website = tk.Entry(width=35)
+entry_email = tk.Entry(width=35)
+entry_password = tk.Entry(width=21)
 
 button_generate = tk.Button(text="Generate Password")
 button_add = tk.Button(text="Add", width=36)
@@ -28,14 +28,14 @@ button_add = tk.Button(text="Add", width=36)
 
 # Grid layout
 canvas.grid(row=0, column=1)
-label_website.grid(row=1, column=0)
-label_email.grid(row=2, column=0)
-label_password.grid(row=3, column=0)
-text_website.grid(row=1, column=1, columnspan=2)
-text_email.grid(row=2, column=1, columnspan=2)
-text_password.grid(row=3, column=1)
-button_generate.grid(row=3, column=2)
-button_add.grid(row=4, column=1, columnspan=2)
+label_website.grid(row=1, column=0, sticky=tk.E)
+label_email.grid(row=2, column=0, sticky=tk.E)
+label_password.grid(row=3, column=0, sticky=tk.E)
+entry_website.grid(row=1, column=1, columnspan=2, sticky=tk.EW)
+entry_email.grid(row=2, column=1, columnspan=2, sticky=tk.EW)
+entry_password.grid(row=3, column=1, sticky=tk.EW)
+button_generate.grid(row=3, column=2, sticky=tk.EW)
+button_add.grid(row=4, column=1, columnspan=2, sticky=tk.EW)
 
 
 # ---------------------
